08e85139edb135e32d774df91813ffd297d4649c
Jennifer Ye <yej52@mcmaster.ca>
2022-09-18 00:49:54 -0400

    updates line and station objects to get key and automatically set the rest of the values

--- _dataset/Station.py
+++ _dataset/Station.py
@@ -1,24 +1,6 @@
 class Station: 
 
-    def __init__(self,latitude,longtitude,name,display_name,zone,total_lines,rail) -> None:
-        self.longtitude = longtitude
-        self.name = name
-        self.display_name = display_name
-        self.zone = zone
-        self.total_lines = total_lines
-        self.rail=rail
-
-    def get_longitude(self):
-        return self.longtitude
-
-    def get_name(self):
-        return self.name
-    
-    def get_display_name(self):
-        return self.display_name
-
-    def get_zone(self):
-        return self.zone
-    
-    def get_total_lines(self):
-        return self.total_lines
+    def __init__(self,key,other={}) -> None:
+        self.id = key
+        for key in other.keys():
+            setattr(self, key, other[key])
