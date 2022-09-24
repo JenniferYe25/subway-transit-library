a981d34995e2d3a1c5592b151188d0c404f35bc3
Jennifer Ye <yej52@mcmaster.ca>
2022-09-24 17:31:44 -0400

    merged all graph objs into one file

--- GraphObjects/Line.py
+++
@@ -1,8 +0,0 @@
-class Line:
-    def __init__(self,line,other={}) -> None:
-        self.line = line
-        for key in other.keys():
-            setattr(self, key, other[key])
-    
-    def _detail(self):
-        print("line: ", self.line," name:", self.name, "colour: ", self.colour, "stripe: ", self.stripe)
