41acf165403171ba52ecc77259f1880c2b751bfe
Jennifer Ye <yej52@mcmaster.ca>
2022-09-24 17:31:44 -0400

    merged all graph objs into one file

--- GraphObjects/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__=['Edge','Line','Station']
--- GraphObjts.py
+++ GraphObjts.py
@@ -25,3 +25,19 @@ class WeightedEdge(Edge):
         details={"start":self.start, "end":self.end, "weight":self.weight, "line":self.line}
         return details
         
+
+class Line:
+    def __init__(self,line,other={}) -> None:
+        self.line = line
+        for key in other.keys():
+            setattr(self, key, other[key])
+    
+    def _detail(self):
+        print("line: ", self.line," name:", self.name, "colour: ", self.colour, "stripe: ", self.stripe)
+
+
+class Station: 
+    def __init__(self,key,other={}) -> None:
+        self.id = key
+        for key in other.keys():
+            setattr(self, key, other[key])
