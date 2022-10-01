82f953eba3d6e0669c2bcdf6a229d36a24cf4f56
Jennifer Ye <yej52@mcmaster.ca>
2022-10-01 12:01:21 -0400

    removes unused import

--- PathFinders/Dijkstra.py
+++ PathFinders/Dijkstra.py
@@ -1,5 +1,4 @@
 import heapq
-from importlib.resources import path
 import sys, os
 sys.path.append(os.path.normpath(os.path.join(
     os.path.dirname(os.path.abspath(__file__)),'..','PathFinders')))
